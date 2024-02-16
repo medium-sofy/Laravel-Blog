788edc6d205441a0f326e7b5bd096a3d657e70d8
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-16 23:03:17 +0200

    7-Add a route to view a single post

---
+++ resources/views/post.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="/app.css">
+<body>
+  <article><a href="/post"><h1>My First Post</h1></a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nemo tenetur id possimus ipsam quo quisquam repellat officiis, expedita deserunt autem commodi, alias fugit sapiente inventore reiciendis! Dolores, voluptatibus cum?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque ex debitis nisi vero fugit numquam libero veritatis facilis sunt magnam. Culpa voluptate inventore enim accusantium perspiciatis totam velit ad in.</p>
+  </article>
+  <a href="/">Go back</a>
+</body>
