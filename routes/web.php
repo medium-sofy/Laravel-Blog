837e9b3189713b06cce488149b6f0cff19308a3b
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-17 15:10:23 +0200

    10-Cache post contents to memory

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,15 @@ Route::get('/', function () {
 
 // Using '{}' in the route, is considered a wildcard, meaning that any string written in place of {post} will be matched and passed to the closure as a parameter, in this case, its called $slug
 Route::get('/posts/{post}', function($slug){
-  $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
+ 
   // remember to always check if the fiven path exists
-  if(! file_exists($path))
+  if(! file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html"))
   {
     return redirect('/');
   }
-  $post = file_get_contents($path);
-  return view('post',
-  [
-    'post' => $post
-  ]
-);
+
+  $post = cache()->remember("post.{$slug}" , 5 , fn() => file_get_contents($path));  
+  
+  return view('post',['post' => $post]);
+
 })->where('post', '[A-z_\-]+'); // Only allow upper/Lowercase letters as well as dashes and underscores
