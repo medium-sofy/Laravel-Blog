6c5c74c1d7b041ee0530956c600b30b9a78da240
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-17 14:09:48 +0200

    8-Update the links to each post view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,13 +3,13 @@
   <link rel="stylesheet" href="/app.css">
 </head>
 <body>
-  <article><a href="/post"><h1>My First Post</h1></a>
+  <article><a href="/posts/my-first-post"><h1>My First Post</h1></a>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nemo tenetur id possimus ipsam quo quisquam repellat officiis, expedita deserunt autem commodi, alias fugit sapiente inventore reiciendis! Dolores, voluptatibus cum?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque ex debitis nisi vero fugit numquam libero veritatis facilis sunt magnam. Culpa voluptate inventore enim accusantium perspiciatis totam velit ad in.</p>
 </article>
-<article><a href="/post"><h1>My Second Post</h1></a>
+<article><a href="/posts/my-second-post"><h1>My Second Post</h1></a>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nemo tenetur id possimus ipsam quo quisquam repellat officiis, expedita deserunt autem commodi, alias fugit sapiente inventore reiciendis! Dolores, voluptatibus cum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae veniam illo aut, ullam facilis autem eum dolores, iure maxime impedit rem numquam voluptatem corrupti mollitia. Ex beatae ullam officia sequi?</p>
 </article>
-<article><a href="/post"><h1>My Second Post</h1></a>
+<article><a href="/posts/my-third-post"><h1>My Second Post</h1></a>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nemo tenetur id possimus ipsam quo quisquam repellat officiis, expedita deserunt autem commodi, alias fugit sapiente inventore reiciendis! Dolores, voluptatibus cum?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, doloremque! Nostrum, fugit. Molestias earum accusamus possimus laboriosam numquam vero voluptatum. Quo delectus provident nostrum neque molestiae quaerat laudantium repellendus maxime?</p>
 </article>
 </body>
