a9ed83bdfacf2136aae00b18367ef3b236118efd
Mohamed Sofy <mediumsofy@gmail.com>
2024-02-18 22:57:22 +0200

    11-Add all() method to Post model class

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,8 +1,15 @@
 <?php
 namespace App\Models;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
 class Post
 {
+    public static function all()
+    {
+         $files = File::files(resource_path('posts/'));
+         return array_map(fn($file)=> $file->getcontents() ,$files);
+    }
     public static function find($slug)
     {
     if(! file_exists($path = resource_path("posts/{$slug}.html")))
